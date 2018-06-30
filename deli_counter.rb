3e11afff6b9b8456fc354a7a591bcd357c713c05
Ana Bebic <anabebic.x@gmail.com>
2018-06-30 10:08:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,53 +1,38 @@
-katz_deli = []
+katz_deli = [] 
 
-def line(katz_deli)
-  line_positions = [];
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    number = 1
-    katz_deli.each do |customer|
-      line_positions << "#{number}. #{customer}"
-      number += 1
-    end
-    puts "The line is currently: #{line_positions.join(" ")}"
-  end
-end
-
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-      line_positions = katz_deli.index(name)
-      puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
-  return name, line_positions 
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
 end 
-      
+
 take_a_number(katz_deli, "Grace")
 take_a_number(katz_deli, "Tom")
 take_a_number(katz_deli, "Alan")
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(array) 
+  if array.length == 0 
     puts "The line is currently empty."
-    
-    else 
+  else 
     message = "The line is currently:"
-    katz_deli.each_with_index do |value, index|
-      
-      message += " #{index.to_i+1}. #{value}"
-    end 
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
   puts "#{message}"
   end 
-  
-  line(katz_deli)
+end 
+ 
+line(katz_deli)
 
 
-def now_serving(katz_deli)
-  if katz_deli.empty? == true 
+def now_serving(array)
+  if array.empty? == true 
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.empty? == false 
-    puts "The line is currently: #{katz_deli.shift}."
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
   end 
 end 
 
 now_serving(katz_deli)
-end 
