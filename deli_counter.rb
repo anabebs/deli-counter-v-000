be5da6b99d8487835256c9045693f759ba1a459b
Ana Bebic <anabebic.x@gmail.com>
2018-06-30 10:05:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,6 +28,9 @@ take_a_number(katz_deli, "Alan")
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+    
+    else 
+    message = "The line is currently:"
     katz_deli.each_with_index do |value, index|
       
       message += " #{index.to_i+1}. #{value}"
