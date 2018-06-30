dc151d8383e0091c06e7945afd1ee04936a2037b
Ana Bebic <anabebic.x@gmail.com>
2018-06-30 09:46:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,7 @@ def take_a_number(katz_deli, name)
   return name, position 
 end 
       
-      
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Tom")
+take_a_number(katz_deli, "Alan")     
       
